a2b0ba3d58e2acbbab9e63a7a9a577e0db932ecb
munale <lanumjr@gmail.com>
2015-05-22 22:46:07 -0500

    through chapter 3

--- examples from Coursera class.R
+++ examples from Coursera class.R
@@ -94,7 +94,7 @@ mean(hw1$Solar.R, na.rm = TRUE)
 
 
 
-x<- list(a= 1:45, b = rnorm(10), c = 6:60)
+x<- list(a= 1:45, b = rnorm(10), c = 6:60)       # using lapply to apply functions across items in list
 lapply(x, mean)
 
 y <- 1:4
@@ -102,24 +102,24 @@ lapply(y, runif)
 
 sapply(x, mean)
 
-z <- matrix(rnorm(140), 14,10)
+z <- matrix(rnorm(140), 14,10)   # using apply to take mean across rows and colums of matrix
 apply(z, 1, mean)
 apply(z, 2, mean)
 apply(z, 1, quantile, probs = c(.25, .5, .75))
 
-L1 <- list(1,2,3,4,5)
+L1 <- list(1,2,3,4,5)     # using mapply to apply max function across pairwise elements of two lists 
 L2 <- list(5,4,3,2,1)
 mapply(max, L1, L2)
 
 
-d <- c(rnorm(20), runif(20), rnorm(20,6))
+d <- c(rnorm(20), runif(20), rnorm(20,6))    # using tapply to find mean and range of list for each grouping within list 
 e <- gl(3, 20)
 e
 tapply(d, e, mean)
 tapply(d, e, range)
 split(d, e)
 
-library(datasets)
+library(datasets)              #using split to split a dataset by value in the month column. then can use apply functions to take means of columns for each month 
 airs <- split(airquality, airquality$Month)
 lapply(airs, colMeans)
 sapply(airs, colMeans)
@@ -140,3 +140,47 @@ sample(1:10, 2, replace=TRUE)
 
 
 
+e<- rnorm(100, 0, 2)            #using R to simulate a quadratic model using rnorm to model error term and random variable
+x<- rnorm(100)
+y <- 2 - 40*x + 2.4 *x^2 + e 
+summary(y)
+
+
+pnorm(1) # cumulative distribution
+dnorm(1) # density function for less than x
+
+
+# using str to return info on dataframe
+a <- c(1,4,8,9)
+b<- c(3,5,4,9)
+d <- c(88,88,3,44)
+
+df <- rbind(a,b,d)
+str(df)
+ddf <- cbind(a,b,d)
+str(ddf)
+
+# sampling without and with repalcement 
+sample(letters, 6)
+sample(1:100, 8)
+sample(1:10, 4, replace=TRUE)
+
+
+# using R to download a datafile from a website and reading into R
+fileURL <- .....com
+download.file(url, destfile=, method="curl")
+mydata <- read.csv or read.table("destfile")
+
+
+# dataframe tools
+summary()
+str()
+table()
+colSums()
+rowSums()
+table(var %in% c(123))
+DF[DF$var %in% c(123),]
+xt <- xtabs(Freq ~ var1 +var2, data=DF)
+try creating new variables based on a condtion. df$new var = ifelse(df$old > 0, true, false)
+melt()
+dcast(df, id ~ variable)
